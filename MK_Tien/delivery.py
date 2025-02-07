ef8788b4c36b263331ea1a644fd7285d96685f48
phamducai <phamducaihcmut2009@gmail.com>
2025-02-07 08:23:16 +0700

    Update delivery.py

--- MK_Tien/delivery.py
+++ MK_Tien/delivery.py
@@ -45,58 +45,66 @@ end_date = end_of_last_week.strftime("%Y-%m-%d")
 # Truy vấn SQL sử dụng khoảng thời gian được tính toán
 query = f"""
 SELECT
-    b.Stk_ID- 1 AS StoreID,
-    STK_Name AS StoreName,
-   '"' +a.Trans_No,
-    CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ) AS DATE,
-    datepart( hh, CONVERT ( TIME, EfTran_Date, 108 ) ) AS HOUR,
-    datepart( WW, EfTran_Date ) AS Week,
-    b.Goods_ID,
-    Short_Name,
-    Goods_Grp_ID,
-    GGrp_Name,
-    SUM ( SKUBase_Qty ) AS Qty,
-    SUM ( Sales_Amt + VAT_Amt ) AS Amount,
-    SUM ( Direct_Disc_Amt ) AS Discount,
-    Request_Data AS Provider,
-    Response_Data AS 'Ma van don' 
+  b.Stk_ID- 1 AS StoreID,
+  STK_Name AS StoreName,
+  '"' + a.Trans_No AS Trans_No,
+  CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ) AS DATE,
+  datepart( hh, CONVERT ( TIME, EfTran_Date, 108 ) ) AS HOUR,
+  datepart( WW, EfTran_Date ) AS Week,
+  b.Goods_ID,
+  Short_Name,
+  Goods_Grp_ID,
+  GGrp_Name,
+  SUM ( SKUBase_Qty ) AS Qty,
+  SUM ( Sales_Amt + VAT_Amt ) AS Amount,
+  SUM ( Direct_Disc_Amt ) AS Discount,
+  Request_Data AS Provider,
+  Response_Data AS 'Ma van don' 
 FROM
-    STr_Payment a
-    JOIN STr_SaleDtl b ON a.Trans_No= b.Trans_No
-    JOIN Stock c ON b.Stk_ID= Node_ID
-    JOIN Goods d ON b.Goods_ID= d.Goods_ID
-    JOIN GoodsGrp e ON Goods_Grp_ID = GGrp_ID 
+  STr_Payment a
+  JOIN STr_SaleDtl b ON a.Trans_No= b.Trans_No
+  JOIN Stock c ON b.Stk_ID= Node_ID
+  JOIN Goods d ON b.Goods_ID= d.Goods_ID
+  JOIN GoodsGrp e ON Goods_Grp_ID = GGrp_ID 
 WHERE
-    CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ) >= '{start_date}' 
-    AND CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ) <= '{end_date}'
-    AND Pmt_ID = 12 
-    AND Request_Data <> 'BTF' 
+  a.Trans_No IN (
+  SELECT
+    Trans_No 
+  FROM
+    STr_SaleDtl 
+  WHERE
+    CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ) >= {start_date} 
+    AND CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ) <= {end_date} 
+    AND Disabled = 0 
+  ) 
+  AND Pmt_ID = 12 
+  AND Request_Data <> 'BTF' 
 GROUP BY
-    b.Stk_ID- 1,
-    STK_Name,
-    a.Trans_No,
-    CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ),
-    datepart( hh, CONVERT ( TIME, EfTran_Date, 108 ) ),
-    datepart( WW, EfTran_Date ),
-    b.Goods_ID,
-    Short_Name,
-    Goods_Grp_ID,
-    GGrp_Name,
-    Request_Data,
-    Response_Data 
+  b.Stk_ID- 1,
+  STK_Name,
+  a.Trans_No,
+  CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ),
+  datepart( hh, CONVERT ( TIME, EfTran_Date, 108 ) ),
+  datepart( WW, EfTran_Date ),
+  b.Goods_ID,
+  Short_Name,
+  Goods_Grp_ID,
+  GGrp_Name,
+  Request_Data,
+  Response_Data 
 ORDER BY
-    b.Stk_ID- 1,
-    STK_Name,
-    a.Trans_No,
-    CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ),
-    datepart( hh, CONVERT ( TIME, EfTran_Date, 108 ) ),
-    datepart( WW, EfTran_Date ),
-    b.Goods_ID,
-    Short_Name,
-    Goods_Grp_ID,
-    GGrp_Name,
-    Request_Data,
-    Response_Data
+  b.Stk_ID- 1,
+  STK_Name,
+  a.Trans_No,
+  CONVERT ( datetime, LEFT ( EfTran_Date, 12 ), 101 ),
+  datepart( hh, CONVERT ( TIME, EfTran_Date, 108 ) ),
+  datepart( WW, EfTran_Date ),
+  b.Goods_ID,
+  Short_Name,
+  Goods_Grp_ID,
+  GGrp_Name,
+  Request_Data,
+  Response_Data
 """
 
 # Đọc dữ liệu từ SQL Server
